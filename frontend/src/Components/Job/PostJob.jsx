7f1ddc7d0d113aade2dd52a0f474e3917db121a4
spidy2301 <shresthgadhwal@gmail.com>
2024-07-23 23:32:03 +0530

    setting home,login,register page

--- frontend/src/Components/Job/PostJob.jsx
+++ frontend/src/Components/Job/PostJob.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PostJob = () => {
   return (
-    <div>PostJob</div>
+    <div></div>
   )
 }
 
--- frontend/src/Components/Layout/Footer.jsx
+++ frontend/src/Components/Layout/Footer.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import {  FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import "./Footer.css"
 const Footer = () => {
+  const { isAuthorized } = useContext(Context);
   return (
-    <div></div>
-  )
-}
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved By Job Board.</div>
+      <div>
+          
+        <Link /*to={"https://www.linkedin.com/in/shiva-pandey-9b19b4250/"} */target="_blank">
+          <FaLinkedin />
+        </Link>
+        <Link /*to={"https://www.instagram.com/_shiva__pandey/"}*/ target="_blank">
+          <RiInstagramFill />
+        </Link>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
